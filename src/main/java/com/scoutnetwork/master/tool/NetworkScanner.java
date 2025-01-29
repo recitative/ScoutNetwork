1b3c271290d95c145ea1f1af149548116db6c47f
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-01-29 12:27:20 +0300

    Update NetworkScanner.java

--- src/main/java/com/scoutnetwork/master/tool/NetworkScanner.java
+++ src/main/java/com/scoutnetwork/master/tool/NetworkScanner.java
@@ -4,9 +4,9 @@ import com.scoutnetwork.master.style.ConsoleColor;
 
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /*
 @author Sma1lo
@@ -16,27 +16,24 @@ public class NetworkScanner {
     public static void scanNetwork(Scanner scanner) {
         System.out.print("Enter the subnet (192.168.1.): ");
         String subnet = scanner.nextLine();
-        List<Thread> threads = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(20);
 
         for (int i = 1; i < 255; i++) {
             String host = subnet + i;
-            Thread thread = new Thread(() -> checkHost(host));
-            threads.add(thread);
-            thread.start();
+            executor.submit(() -> checkHost(host));
         }
 
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                System.out.println(ConsoleColor.RED + "[ERROR]" + ConsoleColor.RESET + " Thread interrupted: " + e.getMessage());
-            }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
         }
     }
 
     private static void checkHost(String host) {
-        try (Socket socket = new Socket(host, 80)) {
-            System.out.println(ConsoleColor.GREEN + "[INFO] " + ConsoleColor.RESET + host + " available");
+        try {
+            Thread.sleep(100);
+            try (Socket socket = new Socket(host, 80)) {
+                System.out.println(ConsoleColor.GREEN + "[INFO] " + ConsoleColor.RESET + host + " available");
+            }
         } catch (Exception e) {
         }
     }
