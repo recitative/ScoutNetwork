1181c3260a51193e05b3c08289ce03d219763181
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-01-29 12:13:07 +0300

    Update NetworkScanner.java

--- src/main/java/com/scoutnetwork/master/tool/NetworkScanner.java
+++ src/main/java/com/scoutnetwork/master/tool/NetworkScanner.java
@@ -3,6 +3,9 @@ package com.scoutnetwork.master.tool;
 import com.scoutnetwork.master.style.ConsoleColor;
 
 import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /*
@@ -13,17 +16,28 @@ public class NetworkScanner {
     public static void scanNetwork(Scanner scanner) {
         System.out.print("Enter the subnet (192.168.1.): ");
         String subnet = scanner.nextLine();
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 1; i < 255; i++) {
             String host = subnet + i;
+            Thread thread = new Thread(() -> checkHost(host));
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
             try {
-                InetAddress address = InetAddress.getByName(host);
-                if (address.isReachable(1000)) {
-                    System.out.println(ConsoleColor.GREEN + "[INFO] " + ConsoleColor.RESET + host + " available");
-                }
-            } catch (Exception e) {
-                System.out.println(ConsoleColor.RED + "[ERROR]" + ConsoleColor.RESET +"While checking " + host + ": " + e.getMessage());
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println(ConsoleColor.RED + "[ERROR]" + ConsoleColor.RESET + " Thread interrupted: " + e.getMessage());
             }
+        }
+    }
+
+    private static void checkHost(String host) {
+        try (Socket socket = new Socket(host, 80)) {
+            System.out.println(ConsoleColor.GREEN + "[INFO] " + ConsoleColor.RESET + host + " available");
+        } catch (Exception e) {
         }
     }
 }
