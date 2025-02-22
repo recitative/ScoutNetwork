bd7164f20c5cb8521792524f8d7b91a009186837
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-02-22 12:06:56 +0300

    Update ConsoleColor.java

--- src/main/java/com/scoutnetwork/master/style/ConsoleColor.java
+++ src/main/java/com/scoutnetwork/master/style/ConsoleColor.java
@@ -7,9 +7,10 @@ package com.scoutnetwork.master.style;
 public class ConsoleColor {
 
     public static final String RESET = "\u001B[0m";
-
-    public static final String GREEN = "\u001B[32m";
+
     public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
 
     private ConsoleColor() {
